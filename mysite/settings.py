a7c360481fbe963dcc3a9096a511c611f011a3ad
こりす村長 <circle39siki@gmail.com>
2024-06-23 10:03:03 +0900

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,4 +1,9 @@
 INSTALLED_APPS = [
-    ...
-    'measurements',
+'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'measurements', # ここに measurements を追加します
 ]
